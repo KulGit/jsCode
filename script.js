1f6fd190240aafe3c8e620697dafd32da9368351
KulGit <exciland@yandex.ru>
2020-06-02 06:55:02 +0300

    first at 02.05.2020 from home

---
+++ script.js
@@ -0,0 +1,11 @@
+let money = +prompt('Enter summ money!');
+let time = +prompt('Enter time in format');
+appData = {
+    budget: money,
+    timeData: time,
+    expenses = {whatIs: howMuch},
+    optionalExpenses = {},
+    income = [],
+    savings = false,
+}
+let i = prompt('Введите обязательную статью расходов');
